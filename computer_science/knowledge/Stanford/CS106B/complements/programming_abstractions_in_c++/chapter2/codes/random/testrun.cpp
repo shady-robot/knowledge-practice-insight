de691afcc97f5f8e5cc7b94174368f65c77d797b
shady-robot <knightdawner@gmail.com>
2021-10-09 22:49:42 +0800

    cs106b: textbook-chapter2 random library implmentation

---
+++ computer_science/knowledge/Stanford/CS106B/complements/programming_abstractions_in_c++/chapter2/codes/random/testrun.cpp
@@ -0,0 +1,20 @@
+/*
+ * testrun.cpp
+ * Copyright (C) 2021 shady <shady@MrRobot.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+#include "random.h"
+
+using namespace std;
+
+int main(){
+    cout << "hello" << endl;
+    cout << randomReal(20, 400) << endl;
+    return 0;
+}
+
+
+
